7e984946b7883bbe335041c2fa7d483160893655
MurasakiArtemis <erwin.hernandezg@gmail.com>
2016-05-12 09:14:55 -0500

    Agregada la descripción de archivos

--- Main.cpp
+++ Main.cpp
@@ -8,28 +8,28 @@ void imprimeError(int opcion, char **argv)
 {
   switch(opcion)
   {
-  case 0:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [<archivo de salida>]" << std::endl;
-    break;
   case 1:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> <divisor>" << std::endl;
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [<archivo de salida>]" << std::endl;
     break;
   case 2:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [<archivo de salida>] [<número de muestras>=10] [<frecuencia>=3000] [ideal]" << std::endl;
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> <divisor>" << std::endl;
     break;
   case 3:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <factor1> <factor2>" << std::endl;
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [<archivo de salida>] [<número de muestras>=10] [<frecuencia>=3000] [ideal]" << std::endl;
     break;
   case 4:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [lenta] [<archivo de salida>] [<opción>=0]" << std::endl;
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <factor1> <factor2>" << std::endl;
     break;
   case 5:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [lenta] [<archivo de salida>]" << std::endl;
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [lenta] [<archivo de salida>] [<opción>=0]" << std::endl;
     break;
   case 6:
-    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [<archivo de salida>]" << std::endl;
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [lenta] [<archivo de salida>]" << std::endl;
     break;
   case 7:
+    std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> [<archivo de salida>]" << std::endl;
+    break;
+  case 8:
     std::cout << "El modo de uso es: " << argv[0] << " " << opcion << " <archivo de entrada> <filtro> [<archivo de salida>]" << std::endl;
     break;
   default:
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
   if(argc < 2)
   {
     std::cout << "El modo de uso es: " << argv[0] << " <opción> [<argumentos de opción>]^+" << std::endl;
-    std::cout << "Opciones:\n0: Copia\n1: División\n2: Simular circuito\n3: Producto\n4: Transformada\n5: Transformada Inversa\n7: Convolución" << std::endl;
+    std::cout << "Opciones:\n0: Describir Archivo\n1: Copia\n2: División\n3: Simular circuito\n4: Producto\n5: Transformada\n6: Transformada Inversa\n8: Convolución" << std::endl;
     return -1;
   }
   int opcion = std::stoi(argv[1]);
@@ -55,6 +55,20 @@ int main(int argc, char *argv[])
   switch(opcion)
   {
   case 0:
+    {
+      if(argc < 3)
+      {
+	imprimeError(opcion, argv);
+	return -1;
+      }
+      for(int i = 2; i < argc; i++)
+      {
+	ArchivoWAVR a(argv[i]);
+	std::cout << a << std::endl;
+      }
+    }
+    break;
+  case 1:
     {
       if(argc < 3 || argc > 4)
       {
@@ -74,7 +88,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW b(a, str);
     }
     break;
-  case 1:
+  case 2:
     {
       if(argc != 4)
       {
@@ -86,7 +100,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW b(a/divisor);
     }
     break;
-  case 2:
+  case 3:
     {
       if(argc < 3 || argc > 7)
       {	
@@ -111,7 +125,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW b(a.simularCircuitoRC(str, numMuestras, frecCorte, argc == 7));
     }
     break;
-  case 3:
+  case 4:
     {
       if(argc != 4)
       {
@@ -123,7 +137,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW c(a*b);
     }
     break;
-  case 4:
+  case 5:
     {
       if(argc < 3 || argc > 6)
       {
@@ -143,7 +157,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW b(a.transformadaFourier(str, rapida, transfOpcion));
     }
     break;
-  case 5:
+  case 6:
     {
       if(argc < 3 || argc > 5)
       {
@@ -160,7 +174,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW b(a.transformadaInversa(str, rapida));
     }
     break;
-  case 6:
+  case 7:
     {
       if(argc < 3 || argc > 4)
       {
@@ -175,7 +189,7 @@ int main(int argc, char *argv[])
       ArchivoWAVW c(b.transformadaInversa(str));
     }
     break;
-  case 7:
+  case 8:
     {
       if(argc < 4 || argc > 5)
       {
